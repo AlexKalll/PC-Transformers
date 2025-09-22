36787e86050dd6794ef31b7c078d624f99fc61f5
AlexKalll <alexkalalw@gmail.com>
2025-09-22 07:01:53 +0300

    refactor: Update the sequence length and dataset name to pubmed

--- Data_preprocessing/config.py
+++ Data_preprocessing/config.py
@@ -3,8 +3,8 @@ class Config:
     VOCAB_SIZE = None
     PAD_ID=None
     EOS_ID=None
-    MAX_LENGTH = 128
-    DATASET_NAME = "ptb"  # "ptb" for penntreebank "opwb" for OpenWebText
+    MAX_LENGTH = 512
+    DATASET_NAME = "pubmed"  # "ptb" for penntreebank "opwb" for OpenWebText
     BASE_DIR = os.path.dirname(os.path.abspath(__file__)) 
     DATA_DIR = os.path.join(BASE_DIR, "Data") 
     TOKENIZER_DIR = os.path.join(BASE_DIR, "tokenizer", "outputs")  
